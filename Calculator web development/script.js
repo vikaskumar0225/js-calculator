6d450befea4a927d0e31f8f8d02c790503271e99
vikas sorout <vikassorout2@gmail.com>
2025-01-08 23:10:44 +0530

    Add files via upload

---
+++ Calculator web development/script.js
@@ -0,0 +1,29 @@
+const display = document.getElementById('display');
+
+function clearDisplay() {
+    display.innerText = '0';
+}
+
+function deleteLast() {
+    if (display.innerText.length > 1) {
+        display.innerText = display.innerText.slice(0, -1);
+    } else {
+        display.innerText = '0';
+    }
+}
+
+function appendNumber(number) {
+    if (display.innerText === '0') {
+        display.innerText = number;
+    } else {
+        display.innerText += number;
+    }
+}
+
+function calculate() {
+    try {
+        display.innerText = eval(display.innerText);
+    } catch {
+        display.innerText = 'Error';
+    }
+}
